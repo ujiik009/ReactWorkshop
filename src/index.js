bc186ec85016a8b8a9b3126fdfffcf8db867d4b2
ikool009 <ikool009@hotmail.com>
2018-04-09 15:04:26 +0700

    createStore example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import createStore from 'redux';
+import {createStore,combineReducers} from 'redux';
+
+
+const initalState={
+    result:0,
+    value:[],
+    age:10
+}
+const recuder = (state=initalState,action)=>{
+   
+    
+    switch (action.type) {
+        case "ADD":
+                state={
+                    ...state,
+                    result:state.result+=action.payload                                
+                }                
+                state.value.push(action.payload)
+            break;
+        case "SUBTRACT":
+           
+            state-=action.payload;
+            break;
+        default:
+            //alert("default "+state)
+            break;
+    }
+    return state;
+}
+
+const store = createStore(combineReducers({recuder}));
+store.subscribe(()=>{
+    console.log(store.getState().result) 
+//    console.log(store.getState());
+});
+store.dispatch({
+    type:"ADD",
+    payload:1500
+});
+store.dispatch({
+    type:"ADD",
+    payload:1500
+});
+store.dispatch({
+    type:"ADD",
+    payload:1500
+});
+
+store.dispatch({
+    type:"ADD",
+    payload:15000
+});
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
